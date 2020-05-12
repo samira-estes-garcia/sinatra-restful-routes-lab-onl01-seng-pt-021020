2b51e8e7e30e908b7de70f4c0f791e0972030f41
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-05-12 20:54:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
     redirect to "/recipes/#{@recipe.id}"
   end
   
-  delete '/recipe/:id' do 
+  delete '/recipes/:id' do 
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.delete 
     redirect to '/recipes'
